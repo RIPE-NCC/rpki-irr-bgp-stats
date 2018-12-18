409d76a342e09ca17145eeb1c607ff8304a779af
Adianto Wibisono <awibisono@ripe.net>
2018-12-18 13:04:37 +0100

    Also produces csv

--- src/main/resources/stats.sh
+++ src/main/resources/stats.sh
@@ -14,38 +14,45 @@ create_todays_stat_dir $stats_dir todays_stat_dir
 echo "Creating todays dir $todays_stat_dir"
 mkdir -p $todays_stat_dir
 
-
 ipv4_bgp_dump="http://www.ris.ripe.net/dumps/riswhoisdump.IPv4.gz";
 
 echo "Getting ris dump";
 cd $todays_stat_dir;
-curl $ipv4_bgp_dump -o risdump-ipv4.txt.gz 2>/dev/null
+curl $ipv4_bgp_dump -o risdump-ipv4.txt.gz 
 gunzip -f risdump-ipv4.txt.gz
 
 echo "Getting nro extended stats";
 nro_ext_stats="https://www.nro.net/wp-content/uploads/apnic-uploads/delegated-extended"
-curl $nro_ext_stats -o nro-ext-stats.txt 2>/dev/null
+curl $nro_ext_stats -o nro-ext-stats.txt 
 
 echo "Getting roas from validator 3";
 all_roas="https://rpki-validator.prepdev.ripe.net/api/export.csv"
-curl -XGET --header 'Accept: text/csv' $all_roas -o all-roas.csv 2>/dev/null
+curl -XGET --header 'Accept: text/csv' $all_roas -o all-roas.csv 
 
 echo "Getting certified resources from validator 3";
 certified_resources="https://rpki-validator.prepdev.ripe.net/api/rpki-objects/certified.csv"
-curl -X GET --header 'Accept: text/csv'  $certified_resources -o certified.csv 2>/dev/null
+curl -X GET --header 'Accept: text/csv'  $certified_resources -o certified.csv 
 
 echo "Generating country adoption"
-$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --country-adoption | column -s',' -t  > $todays_stat_dir/country-adoption.txt  2>/dev/null
-echo "Generating country activation"
-$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --country-activation | column -s',' -t > $todays_stat_dir/country-activation.txt  2>/dev/null
-echo "Generating rir adoption"
-$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --rir-adoption | column -s',' -t  > $todays_stat_dir/rir-adoption.txt  2>/dev/null
-echo "Generating rir activation"
-$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --rir-activation | column -s',' -t > $todays_stat_dir/rir-activation.txt  2>/dev/null
-echo "Generating web page"
-$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv -n > $todays_stat_dir/nro-stats.html 2>/dev/null
+$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --country-adoption > $todays_stat_dir/country-adoption.csv
+cat $todays_stat_dir/country-adoption.csv | column -s',' -t  > $todays_stat_dir/country-adoption.txt  
 
-# Export rsync password in RSYNC_PASSWORD
+echo "Generating country activation"
+$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --country-activation > $todays_stat_dir/country-activation.csv
+cat $todays_stat_dir/country-activation.csv | column -s',' -t > $todays_stat_dir/country-activation.txt  
+
+echo "Generating rir adoption"
+$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --rir-adoption > $todays_stat_dir/rir-adoption.csv
+cat $todays_stat_dir/rir-adoption.csv | column -s',' -t  > $todays_stat_dir/rir-adoption.txt  
+
+echo "Generating rir activation"
+$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --rir-activation  > $todays_stat_dir/rir-activation.csv
+cat $todays_stat_dir/rir-activation.csv | column -s',' -t > $todays_stat_dir/rir-activation.txt  
+
+echo "Generating web page"
+$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv -n > $todays_stat_dir/nro-stats.html 
+
+# Export rsync password in RSYNC_PASSWORD !!! set it up for env variable of whoever doing cron jobs for this (most likely app-admin)
 rsync_target="rpki@dragonstone.ripe.net::rpki"
 
 function rsync_if_not_empty(){
@@ -57,9 +64,10 @@ function rsync_if_not_empty(){
         echo "Failed to create $1, not rsyincing to NRO"
     fi
 }
-
 rsync_if_not_empty country-adoption.txt 
+rsync_if_not_empty country-adoption.csv
 rsync_if_not_empty country-activation.txt 
+rsync_if_not_empty country-activation.csv
 rsync_if_not_empty rir-adoption.txt 
-rsync_if_not_empty rir-activation.txt 
+rsync_if_not_empty rir-activation.csv
 rsync_if_not_empty nro-stats.html 
