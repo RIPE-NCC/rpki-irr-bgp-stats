61d2ab63dfa94b7eb74762b6c7d35f5dc62cec8d
Adianto Wibisono <awibisono@ripe.net>
2018-12-18 11:21:42 +0100

    Stick with one file

--- src/main/resources/stats.sh
+++ src/main/resources/stats.sh
@@ -1,87 +1,65 @@
-#!/usr/bin/env bash
-#
-# Copyright (c) 2015-2107 RIPE NCC
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#   - Redistributions of source code must retain the above copyright notice,
-#     this list of conditions and the following disclaimer.
-#   - Redistributions in binary form must reproduce the above copyright notice,
-#     this list of conditions and the following disclaimer in the documentation
-#     and/or other materials provided with the distribution.
-#   - Neither the name of this software, nor the names of its contributors, nor
-#     the names of the contributors' employers may be used to endorse or promote
-#     products derived from this software without specific prior written
-#     permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
+function create_todays_stat_dir() {
+   local year=`date +%Y`; 
+   local mnth=`date +%m`; 
+   local day=`date +%d`; 
+   echo "Base $1"
+   eval "$2=$1/$year/$mnth/$day"
+}
+
+rpki_stats_bin='/home/awibisono/cert-stats/irrstats-0.1-SNAPSHOT/rpki-irr-bgp-stats'
+stats_dir='/home/awibisono/cert-stats/nro-stat'
+
+create_todays_stat_dir $stats_dir todays_stat_dir
+
+echo "Creating todays dir $todays_stat_dir"
+mkdir -p $todays_stat_dir
 
 
+ipv4_bgp_dump="http://www.ris.ripe.net/dumps/riswhoisdump.IPv4.gz";
 
-STATS_SCRIPT_DIR="/home/app-admin/cert-stats"
+echo "Getting ris dump";
+cd $todays_stat_dir;
+curl $ipv4_bgp_dump -o risdump-ipv4.txt.gz 2>/dev/null
+gunzip -f risdump-ipv4.txt.gz
 
-# Specify the full path to the validator
-VALIDATOR_BIN="$STATS_SCRIPT_DIR/validator/bin/certification-validator"
+echo "Getting nro extended stats";
+nro_ext_stats="https://www.nro.net/wp-content/uploads/apnic-uploads/delegated-extended"
+curl $nro_ext_stats -o nro-ext-stats.txt 2>/dev/null
 
-#
-# Specify the base directory where this script will archive the
-# validation output and summary.
-#
-# The script will store results like this:
-#
-#  /path/to/base/data/dir
-#           /example.tal
-#                 stats-summary.txt
-#                 /20110131   (date)
-#                     validator.log
-#                     validator.err
-#                     roas.csv
-#                     out/... (normal validator out work-directory)
-#                 /... (more dates)
-#           /... (more TALs)
-#
-BASE_DATA_DIR="/ncc/archive2/certstats"
+echo "Getting roas from validator 3";
+all_roas="https://rpki-validator.prepdev.ripe.net/api/export.csv"
+curl -XGET --header 'Accept: text/csv' $all_roas -o all-roas.csv 2>/dev/null
 
-# The script will store the summary output for each tal in a separate file
-# and sync it over to whelk. Do not use trailing slashes.. they are added by the code..
-SUMMARIES_DIR="$BASE_DATA_DIR/summaries"
-RSYNC_TARGET="/export/certcontent/static/statistics"
+echo "Getting certified resources from validator 3";
+certified_resources="https://rpki-validator.prepdev.ripe.net/api/rpki-objects/certified.csv"
+curl -X GET --header 'Accept: text/csv'  $certified_resources -o certified.csv 2>/dev/null
 
-CERTIFIED_RESOURCES_URL=https://rpki-validator.prepdev.ripe.net/api/rpki-objects/certified.csv
+echo "Generating country adoption"
+$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --country-adoption | column -s',' -t  > $todays_stat_dir/country-adoption.txt  2>/dev/null
+echo "Generating country activation"
+$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --country-activation | column -s',' -t > $todays_stat_dir/country-activation.txt  2>/dev/null
+echo "Generating rir adoption"
+$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --rir-adoption | column -s',' -t  > $todays_stat_dir/rir-adoption.txt  2>/dev/null
+echo "Generating rir activation"
+$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv --rir-activation | column -s',' -t > $todays_stat_dir/rir-activation.txt  2>/dev/null
+echo "Generating web page"
+$rpki_stats_bin -b $todays_stat_dir/risdump-ipv4.txt -s $todays_stat_dir/nro-ext-stats.txt -r $todays_stat_dir/all-roas.csv -f $todays_stat_dir/certified.csv -n > $todays_stat_dir/nro-stats.html 2>/dev/null
 
-# curl -s -X GET --header 'Accept: text/csv' $CERTIFIED_RESOURCES_URL > certified-resources.csv
+# Export rsync password in RSYNC_PASSWORD
+rsync_target="rpki@dragonstone.ripe.net::rpki"
 
+function rsync_if_not_empty(){
+    if [ -s $todays_stat_dir/$1 ] 
+    then
+        rsync $todays_stat_dir/$1 $rsync_target
+        echo "Successfully rsynced $1"
+    else
+        echo "Failed to create $1, not rsyincing to NRO"
+    fi
+}
 
-DD=`date +%Y/%m/%d`
-
-for TAL in 'afrinic.tal' 'arin.tal' 'lacnic.tal' 'ripencc.tal' 'localcert.tal' 'apnic.tal' ; do
-    TAL_STATS_DIR="$BASE_DATA_DIR/$TAL"
-    TODAYS_STAT_DIR="$TAL_STATS_DIR/$DD"
-    mkdir -p "$TODAYS_STAT_DIR"
-
-    VALIDATOR_LOG_FILE="$TODAYS_STAT_DIR/validator.log"
-    VALIDATOR_ERR_FILE="$TODAYS_STAT_DIR/validator.err"
-    TAL_STATS_FILE="$TODAYS_STAT_DIR/$TAL.txt"
-
-    $VALIDATOR_BIN -t $STATS_SCRIPT_DIR/tal/$TAL -o $TODAYS_STAT_DIR/out -r $TODAYS_STAT_DIR/roas.csv >$VALIDATOR_LOG_FILE 2>$VALIDATOR_ERR_FILE
-
-    tail -1 $VALIDATOR_LOG_FILE >> $TAL_STATS_FILE
-
-    # will complain about trailing slashes otherwise
-    tar cf $TODAYS_STAT_DIR/validator-work.tar $TODAYS_STAT_DIR/out 2>/dev/null
-    rm -rf $TODAYS_STAT_DIR/out
-done
-
-rsync -qa $SUMMARIES_DIR/* $RSYNC_TARGET
+rsync_if_not_empty country-adoption.txt 
+rsync_if_not_empty country-activation.txt 
+rsync_if_not_empty rir-adoption.txt 
+rsync_if_not_empty rir-activation.txt 
+rsync_if_not_empty nro-stats.html 
